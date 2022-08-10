ad8d49c627bf2fb082bc1f49d33a711418efe861
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-08-09 22:38:31 -0400

    First try to populate the array - not working

--- P5_SlotMachine/Program.cs
+++ P5_SlotMachine/Program.cs
@@ -6,7 +6,23 @@ namespace P5_SlotMachine// Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,,] array3D = new int [3, 3, 3]; //defined the my array 3D
+
+            Random rnd = new Random();
+            int num1 = rnd.Next(1, 11);
+            int num2 = rnd.Next(1, 11);
+            int num3 = rnd.Next(1, 11);
+
+            for (int index = 0 ; index < 3; index++)
+            {
+                array3D[0,0,0] = num1;
+                array3D[0,0,1] = num2;
+                array3D[0,0,2] = num3;
+            }
+
+
+            string test = array3D.ToString();
+            Console.Write(test);
         }
     }
 }
