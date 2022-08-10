d1a7063c81ca8e1eed31e56e8f0653d30b87f776
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-08-09 23:00:33 -0400

    Second try to populate the array (FOR Loops) - not working

--- P5_SlotMachine/Program.cs
+++ P5_SlotMachine/Program.cs
@@ -4,22 +4,26 @@ namespace P5_SlotMachine// Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        static int GetRandomNumber()
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 11);
+            return num;
+        }
         static void Main(string[] args)
         {
             int[,,] array3D = new int [3, 3, 3]; //defined the my array 3D
-
-            Random rnd = new Random();
-            int num1 = rnd.Next(1, 11);
-            int num2 = rnd.Next(1, 11);
-            int num3 = rnd.Next(1, 11);
-
-            for (int index = 0 ; index < 3; index++)
+          
+            for (int row = 0 ; row < 3; row++)
             {
-                array3D[0,0,0] = num1;
-                array3D[0,0,1] = num2;
-                array3D[0,0,2] = num3;
+                for (int column = 0; column < 3; column++)
+                {
+                    for (int item = 0; item < 3; item++)
+                    {
+                        array3D[row, column, item] = GetRandomNumber();
+                    }
+                }
             }
-
 
             string test = array3D.ToString();
             Console.Write(test);
