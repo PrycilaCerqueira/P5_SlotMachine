a26ea233bcd5a640e778244fbb953ddc2fe776cc
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-08-09 21:14:45 -0400

    Add project files.

---
+++ P5_SlotMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P5_SlotMachine// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
