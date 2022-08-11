8df5e691806bdd2ebdb2bf187d7c058c49033bb2
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-08-10 22:50:03 -0400

    Modified the numbers of items on 1st nested FOR. Created a nested FOR to print the Array 3x3 matrix. Added comments.

--- P5_SlotMachine/Program.cs
+++ P5_SlotMachine/Program.cs
@@ -12,22 +12,28 @@ namespace P5_SlotMachine// Note: actual namespace depends on the project name.
         }
         static void Main(string[] args)
         {
-            int[,,] array3D = new int [3, 3, 3]; //defined the my array 3D
-          
+            int[,,] array3D = new int [3, 3, 1]; //defined the my array 3D - 3rows, 3colums, 1item.           
+            
+            //Nested loops to fill up the array with random number
             for (int row = 0 ; row < 3; row++)
             {
                 for (int column = 0; column < 3; column++)
                 {
-                    for (int item = 0; item < 3; item++)
+                    for (int item = 0; item < 1; item++) // This loop can be elimitated and use the direct index of 0.
                     {
                         array3D[row, column, item] = GetRandomNumber();
                     }
                 }
             }
 
-            foreach (int row in array3D)
+            //Nested array to print the array3D in na 3x3 matrix shape
+            for (int row = 0; row < 3; row++) 
             {
-                Console.WriteLine(row);
+                for(int column = 0; column < 3; column++)
+                {
+                    Console.Write($" {array3D[row, column, 0]}"); //Print the numbers in line sepated by a space
+                }
+                Console.WriteLine();//Skip a line to print another set of numbers in line on the next iteraction
             }
         }
     }
