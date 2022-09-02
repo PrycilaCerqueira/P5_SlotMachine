d238cb1a73ce333463810f2297e88d51c10e967a
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-09-01 21:58:48 -0400

    Class

--- P5_SlotMachine/Logic.cs
+++ P5_SlotMachine/Logic.cs
@@ -8,220 +8,229 @@ namespace P5_SlotMachine
 {
     public class Logic
     {
-		/// <summary>
-		/// Nested loops to fill up the array with random numbers
-		/// </summary>
-		/// <param name="grid">Array [n,n]</param>
-		/// <returns> Filled up array</returns>
-		public static int[,] FillUpGrid(int[,] grid)
+        /// <summary>
+        /// Nested loops to fill up the array with random numbers
+        /// </summary>
+        /// <param name="grid">Array [n,n]</param>
+        /// <returns> Filled up array</returns>
+        public static int[,] FillUpGrid(int[,] grid)
         {
-			
-			for (int row = 0; row < grid.GetLength(0); row++)
-			{
-				for (int column = 0; column < grid.GetLength(1); column++)
-				{
-					Random rnd = new Random();
-					int num = rnd.Next(1, 12);
 
-					grid[row, column] = num; //assigns a rdn number to each array item
+            for (int row = 0; row < grid.GetLength(0); row++)
+            {
+                for (int column = 0; column < grid.GetLength(1); column++)
+                {
+                    Random rnd = new Random();
+                    int num = rnd.Next(1, 12);
 
-				}
-			}
-			return grid;
-		}
+                    grid[row, column] = num; //assigns a rdn number to each array item
 
-		public static void MoveGridElements(int[,] grid)
+                }
+            }
+            return grid;
+        }
+
+        public static void MoveGridElements(int[,] grid)
         {
-			int[] newGrid = new int[grid.GetLength(0)];
+            int[] newGrid = new int[grid.GetLength(0)];
 
-			for (int col = 0; col < grid.GetLength(0); col++)
-			{
-				newGrid = CreateColumnsGrid(grid, col);
+            for (int col = 0; col < grid.GetLength(0); col++)
+            {
+                newGrid = CreateColumnsGrid(grid, col);
 
-				int searchNum = 10;
-				int index1 = Array.IndexOf(newGrid, searchNum);
+                int searchNum = 10;
+                int index1 = Array.IndexOf(newGrid, searchNum);
 
-				if (index1 == 0)
-				{
-					for (int i = 0; i < grid.GetLength(0); i++)
-					{
-						int slot = (i + 2) % grid.GetLength(0);
-						newGrid[slot] = newGrid[++slot];
-					}
+                if (index1 == 0)
+                {
+                    for (int i = 0; i < grid.GetLength(0) -1; i++)
+                    {
+                        //	int slot = (i + 2) % grid.GetLength(0);
 
-					/*
+                        newGrid[i] = newGrid[i + 1];
+                    }
+
+                    int slot = 0;
+                    int x;
+
+                    x = slot + 1;
+                    slot++;
+                    x = slot;
+                    x = slot = slot + 1;
+
+                    /*
 					int element = newGrid[0];
 					newGrid[0] = newGrid[1];
 					newGrid[1] = newGrid[2];
 					newGrid[2] = element;
 					*/
-				}
+                }
 
-				if (index1 == 1)
-				{
+                if (index1 == 1)
+                {
 
-					/*
+                    /*
 					int element = newGrid[1];
 					newGrid[1] = newGrid[2];
 					newGrid[2] = newGrid[0];
 					newGrid[0] = element;
 					*/
-				}
+                }
 
-				if (index1 == 2)
-				{
-					/*
+                if (index1 == 2)
+                {
+                    /*
 					int element = newGrid[2];
 					newGrid[2] = newGrid[0];
 					newGrid[0] = newGrid[1];
 					newGrid[1] = element;
 					*/
-				}
+                }
 
-			}					
-		}	
-
-	
-
-		/// <summary>
-		/// It creates a auxiliary 1D array 
-		/// </summary>
-		/// <param name="grid">Two dimensions array</param>
-		/// <param name="row">Number of rows</param>
-		/// <returns>One dimension array</returns>
-		public static int[] CreateRowsGrid(int[,] grid, int row)
-		{
-			int[] newGrid = new int[grid.GetLength(1)];
-
-			for (int col = 0; col < grid.GetLength(1); col++)
-			{
-				newGrid[col] = grid[row, col];
-			}
-			return newGrid;
-		}
-
-		/// <summary>
-		/// It creates a auxiliary 1D array 
-		/// </summary>
-		/// <param name="grid">Two dimensions array</param>
-		/// <param name="col">Number of columns</param>
-		/// <returns>One dimension array</returns>
-		public static int[] CreateColumnsGrid(int[,] grid, int col)
-		{
-			int[] newGrid = new int[grid.GetLength(0)];
-
-			for (int row = 0; row < grid.GetLength(0); row++)
-			{
-				newGrid[row] = grid[row, col];
-			}
-			return newGrid;
-
-		}
-
-		/// <summary>
-		/// It creates a auxiliary 1D array 
-		/// </summary>
-		/// <param name="grid">Two dimensions array</param>
-		/// <param name="diag">Number of diagonals</param>
-		/// <returns>One dimension array</returns>
-		public static int[] CreateDiagonalGrid(int[,] grid, int diag)
-		{
-			int[] newGrid = new int[grid.GetLength(1)];
-								
-			for (int row = 0; row < grid.GetLength(0); row++)
-			{
-				for (int col = 0; col < grid.GetLength(1); col++)
-				{ 
-					if (diag == 0 && row == col)
-					{
-						newGrid[row] = grid[row, col];
-					}
-					else if (diag == 1 && row + col == grid.GetLength(1) - 1)
-                    {
-						newGrid[row] = grid[row, col];
-                    }
-				}
-			}
-			
-			return newGrid;
-		}
+            }
+        }
 
 
-		/// <summary>
-		/// It verifies if the array elements are the same 
-		/// </summary>
-		/// <param name="newGrid">Int array of 3 elements</param>
-		/// <returns>True or False</returns>
-		public static bool ConfirmEqualityOfElements(int[] newGrid)
-		{
-			if (newGrid[0] == newGrid[1] && newGrid[0] == newGrid[2])
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
 
-		}
-
-		/// <summary>
-		/// It calculates the total amount of cash earned in the game
-		/// </summary>
-		/// <param name="result">True or False</param>
-		/// <param name="cash">Int cash</param>
-		/// <returns>The accumulated cash</returns>
-		public static int CalcCash(bool result, int cash)
-		{
-			if (result == true)
-			{
-				cash = cash + 1;
-			}
-			return cash;
-		}
-
-		/// <summary>
-		/// It checks the arrays elements similarities
-		/// </summary>
-		/// <param name="bet">Int bet </param>
-		/// <param name="grid">Array 2D</param>
-		/// <returns>Int cashSum</returns>
-		public static int CheckGridSimilarElements(int bet, int[,] grid)
+        /// <summary>
+        /// It creates a auxiliary 1D array 
+        /// </summary>
+        /// <param name="grid">Two dimensions array</param>
+        /// <param name="row">Number of rows</param>
+        /// <returns>One dimension array</returns>
+        public static int[] CreateRowsGrid(int[,] grid, int row)
         {
-			bool result;
-			int[] newGrid = new int[3];
-			int cashSum = 0;
+            int[] newGrid = new int[grid.GetLength(1)];
 
-			//Check ROWS elements similarities
-			for (int rows = 0; rows < bet; rows++)
-			{
-				newGrid = CreateRowsGrid(grid, rows);
-				result = ConfirmEqualityOfElements(newGrid);
-				cashSum = CalcCash(result, cashSum);
-			}
+            for (int col = 0; col < grid.GetLength(1); col++)
+            {
+                newGrid[col] = grid[row, col];
+            }
+            return newGrid;
+        }
 
-			if (bet > 2)
-			{
-				//Check COLUMNS elements similarities
-				for (int columns = 0; columns < grid.GetLength(0); columns++)
-				{
-					newGrid = CreateColumnsGrid(grid, columns);
-					result = ConfirmEqualityOfElements(newGrid);
-					cashSum = CalcCash(result, cashSum);
-				}
+        /// <summary>
+        /// It creates a auxiliary 1D array 
+        /// </summary>
+        /// <param name="grid">Two dimensions array</param>
+        /// <param name="col">Number of columns</param>
+        /// <returns>One dimension array</returns>
+        public static int[] CreateColumnsGrid(int[,] grid, int col)
+        {
+            int[] newGrid = new int[grid.GetLength(0)];
 
-				//Check DIAGONAL array elements similarities
-				for (int diagonals = 0; diagonals < 2; diagonals++)
-				{
-					newGrid = CreateDiagonalGrid(grid, diagonals);
-					result = ConfirmEqualityOfElements(newGrid);
-					cashSum = CalcCash(result, cashSum);
-				}
-			}
-			return cashSum;
+            for (int row = 0; row < grid.GetLength(0); row++)
+            {
+                newGrid[row] = grid[row, col];
+            }
+            return newGrid;
 
-		}
+        }
 
-	}
+        /// <summary>
+        /// It creates a auxiliary 1D array 
+        /// </summary>
+        /// <param name="grid">Two dimensions array</param>
+        /// <param name="diag">Number of diagonals</param>
+        /// <returns>One dimension array</returns>
+        public static int[] CreateDiagonalGrid(int[,] grid, int diag)
+        {
+            int[] newGrid = new int[grid.GetLength(1)];
+
+            for (int row = 0; row < grid.GetLength(0); row++)
+            {
+                for (int col = 0; col < grid.GetLength(1); col++)
+                {
+                    if (diag == 0 && row == col)
+                    {
+                        newGrid[row] = grid[row, col];
+                    }
+                    else if (diag == 1 && row + col == grid.GetLength(1) - 1)
+                    {
+                        newGrid[row] = grid[row, col];
+                    }
+                }
+            }
+
+            return newGrid;
+        }
+
+
+        /// <summary>
+        /// It verifies if the array elements are the same 
+        /// </summary>
+        /// <param name="newGrid">Int array of 3 elements</param>
+        /// <returns>True or False</returns>
+        public static bool ConfirmEqualityOfElements(int[] newGrid)
+        {
+            if (newGrid[0] == newGrid[1] && newGrid[0] == newGrid[2])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// It calculates the total amount of cash earned in the game
+        /// </summary>
+        /// <param name="result">True or False</param>
+        /// <param name="cash">Int cash</param>
+        /// <returns>The accumulated cash</returns>
+        public static int CalcCash(bool result, int cash)
+        {
+            if (result == true)
+            {
+                cash = cash + 1;
+            }
+            return cash;
+        }
+
+        /// <summary>
+        /// It checks the arrays elements similarities
+        /// </summary>
+        /// <param name="bet">Int bet </param>
+        /// <param name="grid">Array 2D</param>
+        /// <returns>Int cashSum</returns>
+        public static int CheckGridSimilarElements(int bet, int[,] grid)
+        {
+            bool result;
+            int[] newGrid = new int[3];
+            int cashSum = 0;
+
+            //Check ROWS elements similarities
+            for (int rows = 0; rows < bet; rows++)
+            {
+                newGrid = CreateRowsGrid(grid, rows);
+                result = ConfirmEqualityOfElements(newGrid);
+                cashSum = CalcCash(result, cashSum);
+            }
+
+            if (bet > 2)
+            {
+                //Check COLUMNS elements similarities
+                for (int columns = 0; columns < grid.GetLength(0); columns++)
+                {
+                    newGrid = CreateColumnsGrid(grid, columns);
+                    result = ConfirmEqualityOfElements(newGrid);
+                    cashSum = CalcCash(result, cashSum);
+                }
+
+                //Check DIAGONAL array elements similarities
+                for (int diagonals = 0; diagonals < 2; diagonals++)
+                {
+                    newGrid = CreateDiagonalGrid(grid, diagonals);
+                    result = ConfirmEqualityOfElements(newGrid);
+                    cashSum = CalcCash(result, cashSum);
+                }
+            }
+            return cashSum;
+
+        }
+
+    }
 
 }
