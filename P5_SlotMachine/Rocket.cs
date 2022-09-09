44be3bca62c84b95903f0011669cf7f3cc6f966c
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-09-09 11:47:38 -0400

    Implememted Rocket class

---
+++ P5_SlotMachine/Rocket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5_SlotMachine
+{
+    internal class Rocket
+    {
+        public enum Mode
+        {
+            Up = 10,
+            Down = 11,
+        }
+    }
+}
